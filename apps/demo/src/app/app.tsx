da22e742e8f002277ff64e5f0c64095ab8b36768
XuYuCheng <xuyucheng@tezign.com>
2021-08-06 11:26:34 +0800

    feat(demo): add layout

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,9 +1,29 @@
 import { Cropper } from "@react-image-cropper/cropper";
+import { css, cx } from "@emotion/css";
+import { tw } from "twind";
+/* eslint-disable-next-line */
+import styles from "./app.module.css";
+
+// * --------------------------------------------------------------------------- comp
 
 export const App = () => {
   return (
-    <div>
-      <Cropper />
+    <div className={cx(tw`w-screen h-screen flex pt-2 items-center justify-center overflow-hidden`, layout)}>
+      <div className={cx(tw`overflow-auto`, cropperWrapper)}>
+        <Cropper />
+      </div>
     </div>
   );
 };
+
+// * --------------------------------------------------------------------------- style
+
+const layout = css`
+  background-color: #eee;
+`;
+
+const cropperWrapper = css`
+  background-color: #fff;
+  width: 800px;
+  height: 700px;
+`;
